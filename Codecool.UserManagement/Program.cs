277b4eedd50809d9f66ea0caacabb04ec3a19506
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-08-13 15:53:09 +0300

    First stepts with C# - C# basics

--- Codecool.UserManagement/Program.cs
+++ Codecool.UserManagement/Program.cs
@@ -2,11 +2,27 @@
 
 internal class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         string[] names = { "John", "Alexa", "Kyle" };
         int[] ages = { 20, 25, 40 };
         Gender[] genders = { Gender.Male, Gender.Female, Gender.Male };
+        for (int i = 0; i < 3; i++)
+        {   
+            string summarizeSentence = Summarize(names[i], ages[i], genders[i]);
+            DisplaySummary(summarizeSentence);
+        }
+
+        string[] arr = Summarize(names, ages, genders);
+        DisplaySummary(arr);
+
+        if (AskIfUpdateAge()) 
+        {
+            int userIndex = AskForIndex(names.Length);
+            int userAge = AskForNewAge();
+            UpdateAge(userIndex, ages, userAge);
+        }
+        DisplaySummary(Summarize(names, ages, genders));
     }
 
     #region Summary
@@ -14,23 +30,33 @@ internal class Program
     private static string Summarize(string name, int age, Gender gender)
     {
         //TODO: implement
-        return null;
+        return $"{name} [{gender}] - {age} years old";
     }
 
     private static string[] Summarize(string[] names, int[] ages, Gender[] genders)
     {
+        string[] arr = new string[3];
         //TODO: implement
-        return null;
+        for (int i = 0; i < 3; i++)
+        {   
+            arr[i] = $"{names[i]} [{genders[i]}] - {ages[i]} years old";   
+        }
+        return arr;
     }
 
     private static void DisplaySummary(string summary)
     {
         //TODO: implement
+        Console.WriteLine(summary);
     }
 
     private static void DisplaySummary(string[] summaries)
     {
         //TODO: implement
+        for (int i = 0; i < summaries.Length; i++)
+        {
+            Console.WriteLine($"{i+1}: {summaries[i]}");
+        }
     }
 
     #endregion
@@ -40,6 +66,7 @@ internal class Program
     private static void UpdateAge(int index, int[] ages, int newAge)
     {
         //TODO: implement
+        ages[index] = newAge;
     }
 
     #endregion
@@ -49,31 +76,57 @@ internal class Program
     private static string GetInput()
     {
         //TODO: implement
-        return null;
+        return Console.ReadLine() ?? string.Empty;
     }
 
-    private static bool ParseInput(string input, out int number)
+    private static int ParseInput(string input, out int number)
     {
         //TODO: implement
-        return false;
+        number = Int32.Parse(input);
+        return number;
     }
 
     private static int AskForIndex(int max)
     {
         //TODO: implement
-        return 0;
+        Console.Write("What index do you want to update? ");
+        string indexInput = GetInput();
+        try
+        {
+            ParseInput(indexInput, out int parsedInput);
+            if (parsedInput >= 0 && parsedInput <= max-1) return parsedInput;
+            else throw new IndexOutOfRangeException("The index must be between 0 and 2.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return AskForIndex(max);
+        }
     }
 
     private static bool AskIfUpdateAge()
     {
         //TODO: implement
-        return false;
+        Console.Write("Do you want to update age? Yes/No ");
+        string ageInput = GetInput();
+        return ageInput == "Yes";
     }
 
     private static int AskForNewAge()
     {
         //TODO: implement
-        return 0;
+        Console.Write("Type new age: ");
+        string ageInput = GetInput();
+        try 
+        {
+            ParseInput(ageInput, out int parsedAgeInput);
+            return parsedAgeInput;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return AskForNewAge();
+        }
     }
 
     #endregion
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # user-management
+## First steps with C# programming language. We ask user if he wants to change some data and if so, then we do that.
+## We covered some basics of C#: console input/output, methods implementation, type conversions and built-in types.
